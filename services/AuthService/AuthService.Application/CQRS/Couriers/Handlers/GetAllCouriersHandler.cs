88bb5f90e09b6aad9d00ca1bd8a73e14c997a553
redouter <skittels261105@gmail.com>
2026-01-27 11:01:25 +0300

    added getall querys and hadnlers for customers and couriers

---
+++ services/AuthService/AuthService.Application/CQRS/Couriers/Handlers/GetAllCouriersHandler.cs
@@ -0,0 +1,19 @@
+﻿using AuthService.Application.CQRS.Couriers.Queries;
+using AuthService.Application.DTO.Courier;
+using AuthService.Domain.Common;
+using AuthService.Domain.Interfaces;
+using AutoMapper;
+using MediatR;
+
+namespace AuthService.Application.CQRS.Couriers.Handlers;
+
+public class GetAllCouriersHandler(IUnitOfWork unitOfWork, IMapper mapper)
+    : IRequestHandler<GetAllCouriersQuery, PagedList<CourierDto>>
+{
+    public async Task<PagedList<CourierDto>> Handle(GetAllCouriersQuery request, CancellationToken cancellationToken)
+    {
+        var pagedCouriers = await unitOfWork.CourierRepository
+            .GetAllAsync(request.Page, request.PageSize, cancellationToken);
+        return mapper.Map<PagedList<CourierDto>>(pagedCouriers);
+    }
+}
---
+++ services/AuthService/AuthService.Application/CQRS/Customers/Queries/GetAllCustomersQuery.cs
@@ -0,0 +1,8 @@
+﻿using AuthService.Application.DTO.Customers;
+using AuthService.Domain.Common;
+using MediatR;
+
+namespace AuthService.Application.CQRS.Customers.Queries;
+
+public record GetAllCustomersQuery(int Page = 1, int PageSize = 10)
+    : IRequest<PagedList<CustomerDto>>;
