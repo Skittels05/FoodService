b2e1fb945ce0db00dc3e29a66181d839cbfcebb3
redouter <skittels261105@gmail.com>
2026-01-27 10:42:12 +0300

    fixed mapping

--- services/AuthService/AuthService.Application/Mappings/MappingProfile.cs
+++ services/AuthService/AuthService.Application/Mappings/MappingProfile.cs
@@ -1,5 +1,6 @@
 ﻿using AuthService.Application.CQRS.Couriers.Commands;
 using AuthService.Application.CQRS.Customers.Commands;
+using AuthService.Application.CQRS.Users.Commands;
 using AuthService.Application.DTO.Courier;
 using AuthService.Application.DTO.Customers;
 using AuthService.Application.DTO.Users;
@@ -13,13 +14,28 @@ public class MappingProfile : Profile
 {
     public MappingProfile()
     {
-        CreateMap<User, UserAccountDto>();
 
-        CreateMap<Customer, CustomerDto>();
-        CreateMap<CreateCustomerCommand, Customer>();
+        CreateMap(typeof(PagedList<>), typeof(PagedList<>))
+            .ConvertUsing(typeof(PagedListConverter<,>));
+
+        CreateMap<User, UserAccountDto>();
+        CreateMap<CreateUserCommand, User>()
+            .ConstructUsing(src => new User(src.Email, src.UserName, src.Role));
+        CreateMap<UpdateUserCommand, User>()
+            .ForMember(dest => dest.Id, opt => opt.Ignore());
 
         CreateMap<Courier, CourierDto>();
-        CreateMap(typeof(PagedList<>), typeof(PagedList<>)).ConvertUsing(typeof(PagedListConverter<,>));
-        CreateMap<CreateCourierCommand, Courier>();
+        CreateMap<CreateCourierCommand, Courier>()
+            .ConstructUsing(src => new Courier(
+                src.UserId,
+                src.Name,
+                src.VehicleType,
+                src.DocumentsPath,
+                src.PhotoVerificationPath
+            ));
+
+        CreateMap<Customer, CustomerDto>();
+        CreateMap<CreateCustomerCommand, Customer>()
+            .ConstructUsing(src => new Customer(src.UserId, src.Name));
     }
 }
