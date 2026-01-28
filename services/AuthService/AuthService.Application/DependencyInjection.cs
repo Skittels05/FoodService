650819ebe883338b8ee1e597af1079a7448929c4
redouter <skittels261105@gmail.com>
2026-01-28 12:26:44 +0300

    add dependecy injection

---
+++ services/AuthService/AuthService.Application/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AuthService.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        var assembly = Assembly.GetExecutingAssembly();
+        services.AddMediatR(cfg =>
+        {
+            cfg.RegisterServicesFromAssembly(assembly);
+        });
+        services.AddAutoMapper(cfg =>
+            cfg.AddMaps(Assembly.GetExecutingAssembly()));
+        return services;
+    }
+}
