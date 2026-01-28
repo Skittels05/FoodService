09b243f6344fdc423be2989257640633d7f0604f
redouter <skittels261105@gmail.com>
2026-01-28 12:05:52 +0300

    add dbcontext

---
+++ services/AuthService/AuthService.Infrasructure/Persistence/Configurations/CourierConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using AuthService.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AuthService.Infrastructure.Persistence.Configurations;
+
+public class CourierConfiguration : IEntityTypeConfiguration<Courier>
+{
+    public void Configure(EntityTypeBuilder<Courier> builder)
+    {
+        builder.HasKey(c => c.Id);
+        builder.HasIndex(c => c.UserId).IsUnique();
+        builder.HasOne<User>()
+               .WithOne()
+               .HasForeignKey<Courier>(c => c.UserId)
+               .OnDelete(DeleteBehavior.Cascade);
+        builder.Property(c => c.Name)
+            .IsRequired()
+            .HasMaxLength(100);
+        builder.Property(c => c.VehicleType)
+            .HasConversion<string>()
+            .IsRequired();
+        builder.Property(c => c.DocumentsPath).IsRequired();
+        builder.Property(c => c.PhotoVerificationPath).IsRequired();
+    }
+}
