a50dfd8b68416fb5524ce68e506be33f309a7d10
redouter <skittels261105@gmail.com>
2026-02-04 22:17:37 +0300

    replaced swagger with scalar

--- services/AuthService/AuthService.API/Program.cs
+++ services/AuthService/AuthService.API/Program.cs
@@ -1,5 +1,6 @@
 using AuthService.Application;
 using AuthService.Infrastructure;
+using Scalar.AspNetCore;
 
 namespace AuthService.API
 {
@@ -11,23 +12,24 @@ namespace AuthService.API
             builder.Services.AddApplication();
             builder.Services.AddInfrastructure(builder.Configuration);
             builder.Services.AddControllers();
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddOpenApi();
 
             var app = builder.Build();
 
             if (app.Environment.IsDevelopment())
             {
-                app.UseSwagger();
-                app.UseSwaggerUI();
+                app.MapOpenApi();
+                app.MapScalarApiReference(options =>
+                {
+                    options.WithTitle("AuthService API")
+                           .WithTheme(ScalarTheme.Moon)
+                           .WithDefaultHttpClient(ScalarTarget.CSharp, ScalarClient.HttpClient);
+                });
             }
 
             app.UseHttpsRedirection();
-
             app.UseAuthorization();
-
             app.MapControllers();
-
             app.Run();
         }
     }
