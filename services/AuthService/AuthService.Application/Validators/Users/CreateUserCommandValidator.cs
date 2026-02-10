289ba624f267c9f0c416d2cfd98619e5e313d5e7
redouter <skittels261105@gmail.com>
2026-02-10 13:41:30 +0300

    add validators for users

---
+++ services/AuthService/AuthService.Application/Validators/Users/CreateUserCommandValidator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using AuthService.Application.CQRS.Users.Commands;
+using AuthService.Domain.Enums;
+
+namespace AuthService.Application.Validators.Users;
+
+public class CreateUserCommandValidator : AbstractValidator<CreateUserCommand>
+{
+    public CreateUserCommandValidator()
+    {
+        RuleFor(x => x.Email)
+            .NotEmpty().WithMessage("Email is required.")
+            .EmailAddress().WithMessage("A valid email address is required.");
+
+        RuleFor(x => x.UserName)
+            .NotEmpty().WithMessage("Username is required.")
+            .MaximumLength(50).WithMessage("Username must not exceed 50 characters.");
+
+        RuleFor(x => x.Password)
+            .NotEmpty().WithMessage("Password is required.")
+            .MinimumLength(6).WithMessage("Password must be at least 6 characters long.");
+
+        RuleFor(x => x.Role)
+            .IsInEnum().WithMessage("Invalid role.")
+            .NotEqual(UserRole.None).WithMessage("User role must be selected.");
+    }
+}
---
+++ services/AuthService/AuthService.Application/Validators/Users/UpdateUserCommandValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using AuthService.Application.CQRS.Users.Commands;
+
+namespace AuthService.Application.Validators.Users;
+
+public class UpdateUserCommandValidator : AbstractValidator<UpdateUserCommand>
+{
+    public UpdateUserCommandValidator()
+    {
+        RuleFor(x => x.Id)
+            .NotEmpty().WithMessage("User ID is required.");
+
+        RuleFor(x => x.Email)
+            .NotEmpty().WithMessage("Email is required.")
+            .EmailAddress().WithMessage("A valid email address is required.");
+
+        RuleFor(x => x.UserName)
+            .NotEmpty().WithMessage("Username is required.")
+            .MaximumLength(50).WithMessage("Username must not exceed 50 characters.");
+    }
+}
