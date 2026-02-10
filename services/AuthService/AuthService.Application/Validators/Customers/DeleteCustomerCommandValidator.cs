8df821a8063292b4e97007c45d218a95503822be
redouter <skittels261105@gmail.com>
2026-02-10 12:56:25 +0300

    add validators for customer

---
+++ services/AuthService/AuthService.Application/Validators/Customers/DeleteCustomerCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using AuthService.Application.CQRS.Customers.Commands;
+
+namespace AuthService.Application.Validators.Customers;
+
+public class DeleteCustomerCommandValidator : AbstractValidator<DeleteCustomerCommand>
+{
+    public DeleteCustomerCommandValidator()
+    {
+        RuleFor(x => x.Id)
+            .NotEmpty().WithMessage("Customer ID is required.");
+    }
+}
---
+++ services/AuthService/AuthService.Application/Validators/Customers/UpdateCustomerCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using AuthService.Application.CQRS.Customers.Commands;
+
+namespace AuthService.Application.Validators.Customers;
+
+public class UpdateCustomerCommandValidator : AbstractValidator<UpdateCustomerCommand>
+{
+    public UpdateCustomerCommandValidator()
+    {
+        RuleFor(x => x.Id)
+            .NotEmpty().WithMessage("Customer ID is required.");
+
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("Customer name is required.")
+            .MaximumLength(50).WithMessage("Customer name must not exceed 50 characters.");
+    }
+}
