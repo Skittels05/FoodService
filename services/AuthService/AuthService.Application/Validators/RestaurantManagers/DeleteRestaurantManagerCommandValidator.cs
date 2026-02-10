98dc324cb068ae56fd8b6448cce84c5a0345f053
redouter <skittels261105@gmail.com>
2026-02-10 13:34:39 +0300

    add validators fot managers

---
+++ services/AuthService/AuthService.Application/Validators/RestaurantManagers/DeleteRestaurantManagerCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using AuthService.Application.CQRS.RestaurantManagers.Commands;
+
+namespace AuthService.Application.Validators.RestaurantManagers;
+
+public class DeleteRestaurantManagerCommandValidator : AbstractValidator<DeleteRestaurantManagerCommand>
+{
+    public DeleteRestaurantManagerCommandValidator()
+    {
+        RuleFor(x => x.Id)
+            .NotEmpty().WithMessage("Manager ID is required.");
+    }
+}
