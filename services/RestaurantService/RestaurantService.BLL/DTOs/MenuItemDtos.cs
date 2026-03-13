643b0e700b798f04f90d8ef17ba6402523b05f88
redouter <skittels261105@gmail.com>
2026-03-13 15:20:48 +0300

    add dtos

---
+++ services/RestaurantService/RestaurantService.BLL/DTOs/MenuItemDtos.cs
@@ -0,0 +1,5 @@
+﻿namespace RestaurantService.BLL.DTOs;
+
+public record MenuItemDto(Guid Id, Guid RestaurantId, string Name, decimal Price, bool IsActive);
+public record CreateMenuItemDto(string Name, decimal Price, bool IsActive);
+public record UpdateMenuItemDto(string Name, decimal Price, bool IsActive);
---
+++ services/RestaurantService/RestaurantService.BLL/DTOs/StopListDtos.cs
@@ -0,0 +1,4 @@
+﻿namespace RestaurantService.BLL.DTOs;
+
+public record StopListItemDto(Guid Id, Guid LocationId, Guid MenuItemId, string Reason);
+public record AddStopListItemDto(Guid MenuItemId, string Reason);
