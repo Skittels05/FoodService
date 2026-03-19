e45477bb11300b87c1cc27509b6043ca192fac13
redouter <skittels261105@gmail.com>
2026-03-19 12:24:47 +0300

    add interceptor

---
+++ services/RestaurantService/RestaurantService.DAL/Interceptors/EntityInterceptor.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using RestaurantService.BLL.Models;
+
+namespace RestaurantService.DAL.Interceptors;
+
+public class EntityInterceptor : SaveChangesInterceptor
+{
+    public override InterceptionResult<int> SavingChanges(DbContextEventData eventData, InterceptionResult<int> result)
+    {
+        UpdateEntities(eventData.Context);
+        return base.SavingChanges(eventData, result);
+    }
+
+    public override ValueTask<InterceptionResult<int>> SavingChangesAsync(DbContextEventData eventData, InterceptionResult<int> result, CancellationToken cancellationToken = default)
+    {
+        UpdateEntities(eventData.Context);
+        return base.SavingChangesAsync(eventData, result, cancellationToken);
+    }
+
+    private static void UpdateEntities(DbContext? context)
+    {
+        if (context is null) return;
+
+        var now = DateTime.UtcNow;
+
+        foreach (var entry in context.ChangeTracker.Entries<BaseModel>())
+        {
+            if (entry.State is EntityState.Added)
+            {
+                entry.Property(x => x.CreatedAt).CurrentValue = now;
+            }
+            else if (entry.State is EntityState.Modified)
+            {
+                entry.Entity.UpdatedAt = now;
+                entry.Property(x => x.CreatedAt).IsModified = false;
+            }
+        }
+    }
+}
