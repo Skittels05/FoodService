bc5c66d112565f2b35c7fe958ffffa55ab5f1c97
redouter <skittels261105@gmail.com>
2026-03-23 15:54:12 +0300

    add dependecy injection

---
+++ services/RestaurantService/RestaurantService.BLL/DependencyInjection.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using RestaurantService.BLL.Mappers;
+using RestaurantService.BLL.Mappers.Interfaces;
+using RestaurantService.BLL.Services;
+using RestaurantService.BLL.Services.Interfaces;
+using System.Reflection;
+
+namespace RestaurantService.BLL;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddBusinessLogicLayer(this IServiceCollection services)
+    {
+        return services
+            .AddMappers()
+            .AddServices();
+    }
+
+    private static IServiceCollection AddMappers(this IServiceCollection services)
+    {
+        services.AddScoped<IMappingService, MappingService>();
+
+        var mapperTypePairs = Assembly.GetExecutingAssembly().GetTypes()
+            .Where(t => t is { IsClass: true, IsAbstract: false })
+            .SelectMany(t => t.GetInterfaces(), (impl, iface) => new { impl, iface })
+            .Where(x => x.iface.IsGenericType && x.iface.GetGenericTypeDefinition() == typeof(IMapper<,>));
+
+        foreach (var pair in mapperTypePairs)
+        {
+            services.AddScoped(pair.iface, pair.impl);
+        }
+
+        return services;
+    }
+
+    private static IServiceCollection AddServices(this IServiceCollection services)
+    {
+        services.AddScoped<ILocationService, LocationService>();
+        services.AddScoped<IMenuItemService, MenuItemService>();
+        services.AddScoped<IRestaurantDocumentService, RestaurantDocumentService>();
+        services.AddScoped<IRestaurantService, Services.RestaurantService>();
+        services.AddScoped<IStopListService, StopListService>();
+
+        return services;
+    }
+}
---
+++ services/RestaurantService/RestaurantService.DAL/DependencyInjection.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RestaurantService.DAL.Interceptors;
+
+
+namespace RestaurantService.DAL;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddDataAccessLayer(this IServiceCollection services, IConfiguration configuration)
+    {
+        return services
+            .AddPersistence(configuration);
+    }
+
+    private static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+        services.AddScoped<EntityInterceptor>();
+        services.AddDbContext<RestaurantDbContext>((serviceProvider, options) =>
+        {
+            var interceptor = serviceProvider.GetRequiredService<EntityInterceptor>();
+
+            options.UseNpgsql(connectionString)
+                   .AddInterceptors(interceptor);
+        });
+
+        return services;
+    }
+}
