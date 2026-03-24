0cc1c959cfa0a6f1b9ea8431c18fbc835a3bb5d6
redouter <40265675+Skittels05@users.noreply.github.com>
2026-03-24 15:02:03 +0300

    delete an extra line

--- services/RestaurantService/RestaurantService.BLL/Models/Location.cs
+++ services/RestaurantService/RestaurantService.BLL/Models/Location.cs
@@ -7,7 +7,6 @@ public class Location : BaseModel
     public required double Latitude { get; set; }
     public required double Longitude { get; set; }
     public required bool IsAcceptingOrders { get; set; }
-
     public List<StopListItem> StopList { get; set; } = [];
     public Restaurant? Restaurant { get; set; }
 }
