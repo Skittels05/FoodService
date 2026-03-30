e36bae22488d77a999821c70e7fc4bc01da29315
redouter <skittels261105@gmail.com>
2026-03-30 15:22:29 +0300

    add coordinates to CustomerAdresses

--- services/AuthService/AuthService.Application/DTO/CustomerAddress/CustomerAddress.cs
+++ services/AuthService/AuthService.Application/DTO/CustomerAddress/CustomerAddress.cs
@@ -4,5 +4,7 @@ public record CustomerAddressDto(
     Guid Id,
     Guid CustomerId,
     string Address,
+    double Latitude, 
+    double Longitude,
     DateTime? LastUsedAt
 );
--- services/AuthService/AuthService.Domain/Entities/CustomerAddress.cs
+++ services/AuthService/AuthService.Domain/Entities/CustomerAddress.cs
@@ -8,14 +8,18 @@ public class CustomerAddress : EntityBase
     public Guid CustomerId { get; private set; }
     [MaxLength(ValidationConstants.AddressMaxLength)]
     public string Address { get; private set; }
+    public double Latitude { get; private set; }
+    public double Longitude { get; private set; }
     public DateTime? LastUsedAt { get; private set; }
 
     protected CustomerAddress() { }
 
-    public CustomerAddress(Guid customerId, string address)
+    public CustomerAddress(Guid customerId, string address, double latitude, double longitude)
     {
         CustomerId = customerId;
         Address = address.Trim();
+        Latitude = latitude;
+        Longitude = longitude;
     }
 
     public void MarkAsUsed()
