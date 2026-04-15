82b56534f437464d726f51c06eba2afc99928a8a
redouter <skittels261105@gmail.com>
2026-04-15 11:53:00 +0300

    add location validators

---
+++ services/RestaurantService/RestaurantService.API/Validators/LocationValidators.cs
@@ -0,0 +1,70 @@
+﻿using FluentValidation;
+using RestaurantService.API.Constants;
+using RestaurantService.BLL.Constants;
+using RestaurantService.BLL.DTOs;
+
+namespace RestaurantService.API.Validators;
+
+public class GetNearbyLocationsDtoValidator : AbstractValidator<GetNearbyLocationsDto>
+{
+    public GetNearbyLocationsDtoValidator()
+    {
+        RuleFor(x => x.Latitude)
+            .InclusiveBetween(ValidationConstants.LatitudeMin, ValidationConstants.LatitudeMax)
+            .WithMessage(ValidatorMessages.InclusiveBetween);
+
+        RuleFor(x => x.Longitude)
+            .InclusiveBetween(ValidationConstants.LongitudeMin, ValidationConstants.LongitudeMax)
+            .WithMessage(ValidatorMessages.InclusiveBetween);
+
+        RuleFor(x => x.RadiusKm)
+            .GreaterThan(ValidationConstants.RadiusMin)
+            .WithMessage(ValidatorMessages.GreaterThan)
+            .LessThanOrEqualTo(ValidationConstants.RadiusMax)
+            .WithMessage(ValidatorMessages.LessThanOrEqualTo);
+    }
+}
+
+public class CreateLocationDtoValidator : AbstractValidator<CreateLocationDto>
+{
+    public CreateLocationDtoValidator()
+    {
+        RuleFor(x => x.RestaurantId)
+            .NotEmpty().WithMessage(ValidatorMessages.Required);
+
+        RuleFor(x => x.Address)
+            .NotEmpty().WithMessage(ValidatorMessages.Required)
+            .MaximumLength(ValidationConstants.LocationAddressMaxLength)
+            .WithMessage(ValidatorMessages.MaxLength);
+
+        RuleFor(x => x.Latitude)
+            .InclusiveBetween(ValidationConstants.LatitudeMin, ValidationConstants.LatitudeMax)
+            .WithMessage(ValidatorMessages.InclusiveBetween);
+
+        RuleFor(x => x.Longitude)
+            .InclusiveBetween(ValidationConstants.LongitudeMin, ValidationConstants.LongitudeMax)
+            .WithMessage(ValidatorMessages.InclusiveBetween);
+    }
+}
+
+public class UpdateLocationDtoValidator : AbstractValidator<UpdateLocationDto>
+{
+    public UpdateLocationDtoValidator()
+    {
+        RuleFor(x => x.Id)
+            .NotEmpty().WithMessage(ValidatorMessages.Required);
+
+        RuleFor(x => x.Address)
+            .NotEmpty().WithMessage(ValidatorMessages.Required)
+            .MaximumLength(ValidationConstants.LocationAddressMaxLength)
+            .WithMessage(ValidatorMessages.MaxLength);
+
+        RuleFor(x => x.Latitude)
+            .InclusiveBetween(ValidationConstants.LatitudeMin, ValidationConstants.LatitudeMax)
+            .WithMessage(ValidatorMessages.InclusiveBetween);
+
+        RuleFor(x => x.Longitude)
+            .InclusiveBetween(ValidationConstants.LongitudeMin, ValidationConstants.LongitudeMax)
+            .WithMessage(ValidatorMessages.InclusiveBetween);
+    }
+}
--- services/RestaurantService/RestaurantService.BLL/Constants/ValidationConstants.cs
+++ services/RestaurantService/RestaurantService.BLL/Constants/ValidationConstants.cs
@@ -8,4 +8,10 @@ public static class ValidationConstants
     public const int LocationAddressMaxLength = 100;
     public const int MenuItemNameMaxLength = 30;
     public const int StopListReasonMaxLength = 100;
+    public const double LatitudeMin = -90.0;
+    public const double LatitudeMax = 90.0;
+    public const double LongitudeMin = -180.0;
+    public const double LongitudeMax = 180.0;
+    public const double RadiusMin = 0.0;
+    public const double RadiusMax = 100.0;
 }
