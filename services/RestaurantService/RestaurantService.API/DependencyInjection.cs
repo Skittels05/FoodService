1f9e1fc3b00e2db8cc2eb476a4c7410388e62c57
redouter <skittels261105@gmail.com>
2026-04-27 17:44:42 +0300

    add logging for restaurant service

---
+++ services/RestaurantService/RestaurantService.API/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using RestaurantService.API.Filters;
+using RestaurantService.API.Middleware;
+using System.Reflection;
+
+namespace RestaurantService.API;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApiLayer(this IServiceCollection services)
+    {
+        services.AddControllers(options =>
+        {
+            options.Filters.Add<ValidationActionFilter>();
+        });
+
+        services.AddProblemDetails();
+        services.AddExceptionHandler<GlobalExceptionHandler>();
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+        services.AddOpenApi();
+
+        return services;
+    }
+}
