56ce1619eac9a1809745d9f7fb93343888fd4683
redouter <skittels261105@gmail.com>
2026-05-21 15:16:57 +0300

    add eof new line

--- services/DeliveryService/DeliveryService.BLL/Exceptions/BusinessException.cs
+++ services/DeliveryService/DeliveryService.BLL/Exceptions/BusinessException.cs
@@ -5,6 +5,5 @@ public abstract class BusinessException(string message) : Exception(message);
 public class NotFoundException(string name, Guid id) 
     : BusinessException($"{name} with id {id} not found.");
 
-public class CourierStateNotFoundException(Guid courierId) 
+public class CourierStateNotFoundException(Guid courierId)
     : BusinessException($"Courier state for courier id {courierId} not found.");
-    
