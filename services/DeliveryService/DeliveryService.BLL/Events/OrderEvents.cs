077c5f69860e4bcc591a927af3a2441ac3e7affa
redouter <skittels261105@gmail.com>
2026-07-27 21:07:21 +0300

    add events and new exceptions

---
+++ services/DeliveryService/DeliveryService.BLL/Events/OrderEvents.cs
@@ -0,0 +1,11 @@
+﻿using DeliveryService.BLL.Enums;
+using DispatchR.Abstractions.Notification;
+
+namespace DeliveryService.BLL.Events;
+
+public record OrderConfirmedEvent(Guid OrderId) : INotification;
+public record OrderPreparingEvent(Guid OrderId, Guid RestaurantLocationId) : INotification;
+public record OrderReadyForPickupEvent(Guid OrderId, Guid? CourierId) : INotification;
+public record OrderDeliveringEvent(Guid OrderId, Guid CourierId) : INotification;
+public record OrderDeliveredEvent(Guid OrderId) : INotification;
+public record OrderCancelledEvent(Guid OrderId, OrderCancellationReason Reason, string? Comment) : INotification;
