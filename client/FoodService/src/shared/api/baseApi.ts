0686fb9f6aa65df25c9d48ac4650d56b2ec12f89
redouter <skittels261105@gmail.com>
2026-08-12 17:20:25 +0300

    removed axios

--- client/FoodService/src/shared/api/baseApi.ts
+++ client/FoodService/src/shared/api/baseApi.ts
@@ -1,42 +1,18 @@
-import { createApi } from '@reduxjs/toolkit/query/react'
-import axios, { AxiosError, type AxiosRequestConfig } from 'axios'
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { getAccessToken } from './tokenBridge'
-
-export const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
-})
-
-const axiosBaseQuery =
-  () =>
-  async ({ url, method, data, params, headers }: AxiosRequestConfig) => {
-    try {
-      const token = await getAccessToken()
-      
-      const result = await axiosInstance({
-        url,
-        method,
-        data,
-        params,
-        headers: {
-          ...headers,
-          ...(token ? { Authorization: `Bearer ${token}` } : {}),
-        },
-      })
-      return { data: result.data }
-    } catch (axiosError) {
-      const err = axiosError as AxiosError
-      return {
-        error: {
-          status: err.response?.status,
-          data: err.response?.data || err.message,
-        },
-      }
-    }
-  }
 
 export const baseApi = createApi({
   reducerPath: 'api',
-  baseQuery: axiosBaseQuery(),
+  baseQuery: fetchBaseQuery({
+    baseUrl: import.meta.env.VITE_API_URL,
+    prepareHeaders: async (headers) => {
+      const token = await getAccessToken()
+      if (token) {
+        headers.set('Authorization', `Bearer ${token}`)
+      }
+      return headers
+    },
+  }),
   tagTypes: ['User'],
   endpoints: () => ({}),
 })
