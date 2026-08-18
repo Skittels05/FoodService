e25cadf5ccf2e7e55a854134d0d80f59e5155c15
redouter <skittels261105@gmail.com>
2026-08-18 12:28:42 +0300

    removed default values and added on start validations for OutboxOptions

--- services/DeliveryService/DeliveryService.DAL/DependencyInjection.cs
+++ services/DeliveryService/DeliveryService.DAL/DependencyInjection.cs
@@ -9,6 +9,11 @@ public static class DependencyInjection
     
     public static IServiceCollection AddDal(this IServiceCollection services)
     {
+        services.AddOptions<OutboxOptions>()
+            .BindConfiguration(OutboxOptions.SectionName)
+            .ValidateDataAnnotations()
+            .ValidateOnStart();
+        
         services.AddKeyedSingleton<JsonSerializerOptions>(
             OutboxOptions.JsonOptionsKey, 
             new JsonSerializerOptions(JsonSerializerDefaults.Web));
